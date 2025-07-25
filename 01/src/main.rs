1dd8c142b46a5d36ee0bd2f44f92e467078ed72b
Ac5000 <15003210+Ac5000@users.noreply.github.com>
2025-07-24 22:31:21 -0400

    Day 01 Part 2 Complete.

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,5 +1,13 @@
-const DIGITS: [&str; 9] = [
-    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+const DIGITS: [(&str, &str, u32); 9] = [
+    ("one", "1", 1),
+    ("two", "2", 2),
+    ("three", "3", 3),
+    ("four", "4", 4),
+    ("five", "5", 5),
+    ("six", "6", 6),
+    ("seven", "7", 7),
+    ("eight", "8", 8),
+    ("nine", "9", 9),
 ];
 
 fn part1(file_name: &str) -> u32 {
@@ -23,11 +31,51 @@ fn part1(file_name: &str) -> u32 {
     sum
 }
 
+/// Returns the calibration value for the line.
+fn find_num_in_line(line: &str) -> u32 {
+    // Index, value
+    let mut first_digit: (usize, u32) = (line.len(), 0);
+    // Index, value
+    let mut second_digit: (usize, u32) = (0, 0);
+
+    for digit in DIGITS {
+        let first_word = line.find(digit.0);
+        let first_digit_str = line.find(digit.1);
+        let second_word = line.rfind(digit.0);
+        let second_digit_str = line.rfind(digit.1);
+
+        // Check for words.
+        if first_word.is_some_and(|x| x < first_digit.0) {
+            first_digit = (first_word.unwrap(), digit.2);
+        }
+        // Check for digits.
+        if first_digit_str.is_some_and(|x| x < first_digit.0) {
+            first_digit = (first_digit_str.unwrap(), digit.2);
+        }
+
+        // Check for words.
+        if second_word.is_some_and(|x| x >= second_digit.0) {
+            second_digit = (second_word.unwrap(), digit.2);
+        }
+        // Check for digits.
+        if second_digit_str.is_some_and(|x| x >= second_digit.0) {
+            second_digit = (second_digit_str.unwrap(), digit.2);
+        }
+    }
+    let sum = (first_digit.1 * 10) + second_digit.1;
+    // Without zero, sum always has to be at least 11.
+    assert!(sum > 10);
+    sum
+}
+
 fn part2(file_name: &str) -> u32 {
     let file_contents = std::fs::read_to_string(file_name).expect("Couldn't open file");
 
     let mut sum: u32 = 0;
 
+    for line in file_contents.lines() {
+        sum = sum + find_num_in_line(line);
+    }
     sum
 }
 
@@ -43,6 +91,20 @@ mod tests {
     #[test]
     fn part1_example01() {
         assert_eq!(part1("example.txt"), 142);
+    }
+
+    #[test]
+    fn test_find_num_in_line() {
+        assert_eq!(find_num_in_line("two1nine"), 29);
+        assert_eq!(find_num_in_line("eightwothree"), 83);
+        assert_eq!(find_num_in_line("abcone2threexyz"), 13);
+        assert_eq!(find_num_in_line("xtwone3four"), 24);
+        assert_eq!(find_num_in_line("4nineeightseven2"), 42);
+        assert_eq!(find_num_in_line("zoneight234"), 14);
+        assert_eq!(find_num_in_line("7pqrstsixteen"), 76);
+        assert_eq!(find_num_in_line("6sevenpqrstsixteen"), 66);
+        assert_eq!(find_num_in_line("67sevenpqrstsixteen7"), 67);
+        assert_eq!(find_num_in_line("1qlbcrdgg"), 11);
     }
 
     #[test]
