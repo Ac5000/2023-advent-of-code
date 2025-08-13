d56215db0d9a7cb0ab60377b23def22268187453
Ac5000 <15003210+Ac5000@users.noreply.github.com>
2025-08-13 01:00:28 -0400

    Refactored original, starting working on part 2 again.

--- 03/src/main.rs
+++ 03/src/main.rs
@@ -7,6 +7,15 @@ struct Digit {
     value: u32,
 }
 
+impl Digit {
+    fn new(coord: Coord, value: u32) -> Self {
+        Self {
+            coord: coord,
+            value: value,
+        }
+    }
+}
+
 impl fmt::Display for Digit {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "( {} @ {} )", self.value, self.coord)
@@ -54,6 +63,12 @@ impl Number {
     }
 }
 
+impl Number {
+    fn digit_coords(&self) -> impl Iterator<Item = Coord> + '_ {
+        self.digits.iter().map(|digit| digit.coord)
+    }
+}
+
 impl fmt::Display for Number {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "( {} - [", self.value)?;
@@ -65,13 +80,13 @@ impl fmt::Display for Number {
     }
 }
 
-/// Get hashmap of digits.
-fn get_digits(file_contents: &String) -> HashMap<Coord, u32> {
+/// Get hashmap of digits from the grid.
+fn get_digits(schematic: &Grid) -> HashMap<Coord, u32> {
     let mut digits = HashMap::new();
-    for (y, line) in file_contents.lines().enumerate() {
-        for (x, character) in line.chars().enumerate().filter(|(_, c)| c.is_ascii_digit()) {
+    for (chord, character) in schematic.char_map.iter() {
+        if character.is_ascii_digit() {
             match digits.insert(
-                Coord::new(x as i32, y as i32),
+                *chord,
                 character.to_digit(10).expect("failed to convert to digit."),
             ) {
                 None => (),
@@ -82,16 +97,12 @@ fn get_digits(file_contents: &String) -> HashMap<Coord, u32> {
     digits
 }
 
-/// Get hashmap of symbols.
-fn get_symbols(file_contents: &String) -> HashMap<Coord, char> {
+/// Get hashmap of symbols from the grid.
+fn get_symbols(schematic: &Grid) -> HashMap<Coord, char> {
     let mut symbols = HashMap::new();
-    for (y, line) in file_contents.lines().enumerate() {
-        for (x, character) in line
-            .chars()
-            .enumerate()
-            .filter(|(_, c)| !c.is_ascii_digit() && c != &'.')
-        {
-            match symbols.insert(Coord::new(x as i32, y as i32), character) {
+    for (chord, character) in schematic.char_map.iter() {
+        if !character.is_ascii_digit() && character != &'.' {
+            match symbols.insert(*chord, *character) {
                 None => (),
                 Some(e) => panic!("Key was already present and contained: {}", e),
             }
@@ -100,65 +111,38 @@ fn get_symbols(file_contents: &String) -> HashMap<Coord, char> {
     symbols
 }
 
-/// Get vec of digits that touch a symbol.
-fn get_touching_symbols(
-    digits: &HashMap<Coord, u32>,
-    symbols: &HashMap<Coord, char>,
-) -> Vec<Coord> {
-    let mut touching: Vec<Coord> = Vec::new();
-    for digit in digits {
-        let surrounding = digit.0.get_surrounding_coords();
-        for coord in surrounding {
-            match symbols.get(&coord) {
-                None => (),
-                Some(_) => touching.push(*digit.0),
-            }
-        }
-    }
-    touching
-}
-
-/// Get a vec of numbers in the grid.
-fn get_numbers2(file_contents: &String) -> Vec<Number> {
+/// Get a vec of numbers in the grid from the hashmap of digits.
+fn get_numbers(schematic: &Grid, digits: HashMap<Coord, u32>) -> Vec<Number> {
     let mut numbers: Vec<Number> = Vec::new();
-    for (y, line) in file_contents.lines().enumerate() {
+    for y in 0..schematic.max_y + 1 {
         let mut number: Option<Number> = None;
-        for (x, character) in line.chars().enumerate() {
-            if !character.is_ascii_digit() {
-                match number {
-                    None => {
-                        continue;
-                    }
-                    Some(mut num) => {
-                        num.get_value();
-                        numbers.push(num);
-                        number = None;
-                    }
-                }
-                continue;
-            } else {
+        for x in 0..schematic.max_x + 1 {
+            let coord = Coord::new(x, y);
+            if let Some(value) = digits.get(&coord) {
                 match number {
                     None => {
                         let mut num = Number::new();
-                        let digit: Digit = Digit {
-                            coord: Coord::new(x as i32, y as i32),
-                            value: character.to_digit(10).expect("Failed to convert digit."),
-                        };
-                        num.digits.push(digit);
+                        num.digits.push(Digit::new(coord, *value));
                         number = Some(num);
                     }
                     Some(ref mut num) => {
-                        let digit: Digit = Digit {
-                            coord: Coord::new(x as i32, y as i32),
-                            value: character.to_digit(10).expect("Failed to convert digit."),
-                        };
-                        num.digits.push(digit);
+                        num.digits.push(Digit::new(coord, *value));
                     }
                 }
+            } else {
+                match number {
+                    None => {}
+                    Some(mut num) => {
+                        num.get_value();
+                        numbers.push(num);
+                    }
+                }
+                number = None;
             }
         }
+        // Push numbers touching end of row.
         match number {
-            None => (),
+            None => {}
             Some(mut num) => {
                 num.get_value();
                 numbers.push(num);
@@ -168,119 +152,97 @@ fn get_numbers2(file_contents: &String) -> Vec<Number> {
     numbers
 }
 
-/// Get a vec of vec of coordinates that make up the various numbers on grid.
-fn get_numbers(file_contents: &String) -> Vec<Vec<Coord>> {
-    let mut numbers: Vec<Vec<Coord>> = Vec::new();
-    for (y, line) in file_contents.lines().enumerate() {
-        let mut number: Option<Vec<Coord>> = None;
-        for (x, character) in line.chars().enumerate() {
-            if !character.is_ascii_digit() {
-                match number {
-                    None => continue,
-                    Some(ref n) => {
-                        numbers.push(n.to_vec());
-                        number = None;
-                    }
-                }
-            } else {
-                match number {
-                    None => number = Some(vec![Coord::new(x as i32, y as i32)]),
-                    Some(ref mut num) => num.push(Coord::new(x as i32, y as i32)),
-                }
-            }
-        }
-        // Need to push in-process numbers at end of lines.
-        match number {
-            None => (),
-            Some(ref n) => {
-                numbers.push(n.to_vec());
-            }
+/// Get numbers that include a Coord in their vec.
+fn get_touching_numbers(numbers: Vec<Number>, symbols: &HashMap<Coord, char>) -> Vec<Number> {
+    let mut touching_nums: Vec<Number> = Vec::new();
+
+    let mut symbol_coords: Vec<Coord> = Vec::new();
+    for coord in symbols.keys() {
+        for coord2 in coord.get_surrounding_coords() {
+            symbol_coords.push(coord2);
         }
     }
-    numbers
-}
 
-/// Get numbers that include a Coord in their vec.
-fn get_touching_numbers(numbers: &Vec<Vec<Coord>>, touching: Vec<Coord>) -> Vec<Vec<Coord>> {
-    let mut touching_nums: Vec<Vec<Coord>> = Vec::new();
     for number in numbers {
-        for digit in number {
-            if touching.contains(&digit) && !touching_nums.contains(number) {
-                touching_nums.push(number.to_vec());
+        for digit in &number.digits {
+            if symbol_coords.contains(&digit.coord) && !touching_nums.contains(&number) {
+                touching_nums.push(number);
+                break;
             }
         }
     }
     touching_nums
 }
 
-/// Convert vec of coords to a u32 using a HashMap of digits.
-fn convert_coords_to_u32(coords: &Vec<Coord>, digits: &HashMap<Coord, u32>) -> u32 {
-    let mut val: u32 = 0;
-    for (tens, coord) in coords.iter().rev().enumerate() {
-        val =
-            val + (digits.get(coord).expect("coord not found in digits") * 10_u32.pow(tens as u32))
-    }
-    val
-}
-
 /// Get sum of numbers touching a symbol.
 fn part1(file_name: &str) -> u32 {
-    let file_contents = std::fs::read_to_string(file_name).expect("Couldn't open file");
-    let digits = get_digits(&file_contents);
-    let symbols = get_symbols(&file_contents);
-    let numbers = get_numbers(&file_contents);
-    let touching_symbols = get_touching_symbols(&digits, &symbols);
-    let touching_nums = get_touching_numbers(&numbers, touching_symbols);
+    let schematic = Grid::new_from_file(file_name);
+    let digits = get_digits(&schematic);
+    let symbols = get_symbols(&schematic);
+    let numbers = get_numbers(&schematic, digits);
+    let touching_nums = get_touching_numbers(numbers, &symbols);
     let mut sum: u32 = 0;
     for num in &touching_nums {
-        sum = sum + convert_coords_to_u32(&num, &digits);
+        sum = sum + num.value as u32;
     }
     println!("sum: {}", sum);
     sum
 }
 
 /// Get coords for all * symbols.
-fn get_gears(symbols: &HashMap<Coord, char>) -> HashMap<Coord, char> {
-    let mut gears: HashMap<Coord, char> = HashMap::new();
-    for symbol in symbols {
-        if symbol.1 == &'*' {
-            match gears.insert(*symbol.0, *symbol.1) {
-                None => (),
-                Some(e) => panic!("Key was already present and contained: {}", e),
-            }
+fn get_gears(symbols: &HashMap<Coord, char>) -> Vec<Coord> {
+    let mut gears: Vec<Coord> = Vec::new();
+    for (coord, symbol) in symbols.iter() {
+        if symbol == &'*' {
+            gears.push(*coord);
         }
     }
     gears
 }
 
+/// Get numbers that include a surrounding Coord in their digits.
+fn get_numbers_touching_gears(numbers: Vec<Number>, gears: Vec<Coord>) -> Vec<Number> {
+    let mut touching_nums: Vec<Number> = Vec::new();
+
+    let mut symbol_coords: Vec<Coord> = Vec::new();
+    for gear in gears {
+        for coord in gear.get_surrounding_coords() {
+            symbol_coords.push(coord);
+        }
+    }
+
+    for number in numbers {
+        for digit in &number.digits {
+            if symbol_coords.contains(&digit.coord) && !touching_nums.contains(&number) {
+                touching_nums.push(number);
+                break;
+            }
+        }
+    }
+    touching_nums
+}
+
 /// Get gear ratio of pairs of numbers touching a * symbol.
 fn part2(file_name: &str) -> u32 {
-    let file_contents = std::fs::read_to_string(file_name).expect("Couldn't open file");
-    let digits = get_digits(&file_contents);
-    let symbols = get_symbols(&file_contents);
+    let schematic = Grid::new_from_file(file_name);
+    let digits = get_digits(&schematic);
+    let symbols = get_symbols(&schematic);
     let gears = get_gears(&symbols);
-    let numbers = get_numbers(&file_contents);
-    let touching_symbols = get_touching_symbols(&digits, &gears);
-    let touching_nums = get_touching_numbers(&numbers, touching_symbols);
+    let numbers = get_numbers(&schematic, digits);
+    let touching_nums = get_touching_numbers(numbers, &symbols);
     let mut sum: u32 = 0;
     for num in &touching_nums {
-        sum = sum + convert_coords_to_u32(&num, &digits);
+        sum = sum + num.value as u32;
     }
     println!("Gear ratio: {}", sum);
     sum
 }
 
 fn main() {
-    // part1("example.txt");
-    // part1("input.txt");
-    // let file_contents = std::fs::read_to_string("example.txt").expect("Couldn't open file");
-    // let nums = get_numbers2(&file_contents);
-    // for num in nums {
-    //     println!("num:{}", num);
-    // }
-    let grid: Grid = Grid::new_from_file("input.txt");
-    println!("{}", grid);
-    println!("\x1b[33mError\x1b[0m");
+    part1("example.txt");
+    part1("input.txt");
+    part2("example2.txt");
+    part2("input.txt");
 }
 
 #[cfg(test)]
@@ -288,14 +250,29 @@ mod tests {
     use super::*;
 
     #[test]
+    fn part1_example01() {
+        assert_eq!(part1("example.txt"), 4361);
+    }
+
+    #[test]
     fn test_part1() {
         assert_eq!(part1("input.txt"), 539433)
     }
 
     #[test]
+    fn part2_example2() {
+        assert_eq!(part2("example2.txt"), 467835);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("input.txt"), 99999999)
+    }
+
+    #[test]
     fn test_get_digits() {
-        let file_contents = std::fs::read_to_string("example.txt").expect("Couldn't open file");
-        let digits = get_digits(&file_contents);
+        let example_grid = Grid::new_from_file("example.txt");
+        let digits = get_digits(&example_grid);
         assert!(digits.contains_key(&Coord::new(0, 0)));
         assert!(digits.contains_key(&Coord::new(1, 9)));
         assert!(digits.contains_key(&Coord::new(7, 2)));
@@ -306,8 +283,8 @@ mod tests {
 
     #[test]
     fn test_get_symbols() {
-        let file_contents = std::fs::read_to_string("example.txt").expect("Couldn't open file");
-        let symbols = get_symbols(&file_contents);
+        let example_grid = Grid::new_from_file("example.txt");
+        let symbols = get_symbols(&example_grid);
         assert!(symbols.contains_key(&Coord::new(3, 1)));
         assert!(symbols.contains_key(&Coord::new(6, 3)));
         assert!(!symbols.contains_key(&Coord::new(1, 1)));
@@ -315,53 +292,20 @@ mod tests {
 
     #[test]
     fn test_get_numbers() {
-        let file_contents = std::fs::read_to_string("example.txt").expect("Couldn't open file");
-        let numbers = get_numbers2(&file_contents);
-        // let expected = vec![Coord::new(0, 0), Coord::new(1, 0), Coord::new(2, 0)];
-        // let expected2 = vec![Coord::new(7, 5), Coord::new(8, 5)];
-        // let expected3 = vec![Coord::new(7, 2), Coord::new(8, 2), Coord::new(9, 2)];
-        // assert!(numbers.contains(&expected));
-        // assert!(numbers.contains(&expected2));
+        let example_grid = Grid::new_from_file("example.txt");
+        let digits = get_digits(&example_grid);
+        let numbers = get_numbers(&example_grid, digits);
+
         let expected = Number {
             value: 467,
             digits: vec![
-                Digit {
-                    coord: Coord::new(0, 0),
-                    value: 4,
-                },
-                Digit {
-                    coord: Coord::new(1, 0),
-                    value: 6,
-                },
-                Digit {
-                    coord: Coord::new(2, 0),
-                    value: 7,
-                },
+                Digit::new(Coord::new(0, 0), 4),
+                Digit::new(Coord::new(1, 0), 6),
+                Digit::new(Coord::new(2, 0), 7),
             ],
         };
+        println!("numbers:{:#?}", numbers);
         assert_eq!(numbers.len(), 10);
         assert!(numbers.contains(&expected));
-    }
-
-    #[test]
-    fn part1_example01() {
-        assert_eq!(part1("example.txt"), 4361);
-    }
-
-    #[test]
-    fn test_get_surrounding_coords() {
-        let cord = Coord::new(1, 1);
-        assert_eq!(cord.north(), Coord::new(1, 0));
-        let expected: Vec<Coord> = vec![
-            Coord::new(1, 0), // N
-            Coord::new(2, 0), // NE
-            Coord::new(2, 1), // E
-            Coord::new(2, 2), // SE
-            Coord::new(1, 2), // S
-            Coord::new(0, 2), // SW
-            Coord::new(0, 1), // W
-            Coord::new(0, 0), // NW
-        ];
-        assert_eq!(cord.get_surrounding_coords(), expected);
     }
 }
