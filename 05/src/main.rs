2cd21faddc645a9fc34a313bbed23bdb13886b53
Ac5000 <15003210+Ac5000@users.noreply.github.com>
2025-12-02 15:43:45 -0500

    Updated README. Also started on Day 5 but then 2025s came out.

--- 05/src/main.rs
+++ 05/src/main.rs
@@ -1,3 +1,38 @@
+//! Day 05: If You Give A Seed A Fertilizer
+
+/// Convert a seed number to a location number using all the conversions between.
+fn part1(file_name: &str) -> u32 {
+    let file_contents = std::fs::read_to_string(file_name).expect("Couldn't open file");
+    5
+}
+// 4294967295
+// 4196563819
 fn main() {
-    println!("Hello, world!");
+    part1("example.txt");
+    part1("input.txt");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example01() {
+        assert_eq!(part1("example.txt"), 13);
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("input.txt"), 24706);
+    }
+
+    // #[test]
+    // fn part2_example01() {
+    //     assert_eq!(part2("example.txt"), 30);
+    // }
+    //
+    // #[test]
+    // fn test_part2() {
+    //     assert_eq!(part2("input.txt"), 13114317);
+    // }
 }
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # 2023-advent-of-code
- 2023 Advent of Code. Started in 2025 for fun/learning Rust
+
+My attempt at the [2023 Advent of Code](https://adventofcode.com/2023). Started
+in 2025 for fun/learning Rust
+
+Attempting to finish it using Rust this year.
+
+## My [Advent of Code](https://adventofcode.com) attempts
+
+- [2025 - Rust](https://github.com/Ac5000/2025-advent-of-code)
+- [2024 - Python](https://github.com/Ac5000/2024-advent-of-code) - First year I participated.
+- [2023 - Rust](https://github.com/Ac5000/2023-advent-of-code) - Went back after
+finishing 2024 for this one because I had fun.
